aeebbd6ab15a5309e66576168ef040c3c19c36ee
Daniel Ortiz <d.ortich.oficial@gmail.com>
2019-01-25 21:01:36 +0100

    Movimiento Base Creado
    Se mueve hacia los lados de manera simple.

---
+++ Assets/Scripts/Personaje.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public float velocidad;
+    public float fuerzaSalto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            gameObject.transform.Translate(-velocidad * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("d"))
+        {
+            gameObject.transform.Translate(velocidad * Time.deltaTime,0,0);
+        }
+    }
+}
