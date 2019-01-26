1a43d00200d0e2718e8839f8fee921280d16db39
XelleX <33550473+XelleXYT@users.noreply.github.com>
2019-01-26 03:45:18 +0100

    Zoom Camara Instantaneo
    Zoom Camara Instantaneo.
    Añadidos Tags.
    Añadido contador FPS.

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject player;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float zoom;
+
+    private void LateUpdate()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, minX, maxX);
+        float y = Mathf.Clamp(player.transform.position.y, minY, maxY);
+
+        if (player.transform.position.x > gameObject.transform.position.x + 5)
+        {
+            gameObject.transform.position = new Vector3(x - 5, y, gameObject.transform.position.z);
+        }
+        if (player.transform.position.x < gameObject.transform.position.x - 5)
+        {
+            gameObject.transform.position = new Vector3(x + 5, y, gameObject.transform.position.z);
+        }
+
+
+        /*
+        float x = Mathf.Clamp(player.transform.position.x, minX, maxX);
+        float y = Mathf.Clamp(player.transform.position.y, minY, maxY);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+        */
+    }
+}
---
+++ Assets/Scripts/ZoomCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCam : MonoBehaviour
+{
+    public float maxSize;
+    public float minSize;
+    public float augmentionSpeed;
+
+    private bool isZoomed = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "zoom")
+        {
+            if (!isZoomed)
+            {
+                Camera.main.orthographicSize = Mathf.Lerp(maxSize, minSize, Time.deltaTime * augmentionSpeed);
+                isZoomed = true;
+            }
+            else
+            {
+                Camera.main.orthographicSize = Mathf.Lerp(minSize, maxSize, Time.deltaTime * augmentionSpeed);
+                isZoomed = false;
+            }
+            
+        }
+    }
+}
