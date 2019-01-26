7e9c6f361cc60a7fa9add7f647d0c9c6eee45319
Daniel Ortiz <d.ortich.oficial@gmail.com>
2019-01-26 03:43:25 +0100

    El personaje se engancha a los bordes
    El personaje se engancha a los bordes, aun no se reajusta a la altura buena

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -6,12 +6,16 @@ public class Personaje : MonoBehaviour
 {
     public float fuerzaMovimiento;
     public float fuerzaSalto;
+
     private bool puedeSaltar;
+    private int agarrado;
+
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,39 +25,94 @@ public class Personaje : MonoBehaviour
         {
             if(!puedeSaltar)
             {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-(fuerzaMovimiento / 2) * Time.deltaTime,0));
+                rb.AddForce(new Vector2(-(fuerzaMovimiento / 2) * Time.deltaTime,0));
             }
             else
             {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-fuerzaMovimiento * Time.deltaTime,0));
+                rb.AddForce(new Vector2(-fuerzaMovimiento * Time.deltaTime,0));
             }
         }
         if(Input.GetKey("d") || Input.GetKey("right"))
         {
             if(!puedeSaltar)
             {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2((fuerzaMovimiento / 2) * Time.deltaTime,0));
+                rb.AddForce(new Vector2((fuerzaMovimiento / 2) * Time.deltaTime,0));
             }
             else
             {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(fuerzaMovimiento * Time.deltaTime,0));
+                rb.AddForce(new Vector2(fuerzaMovimiento * Time.deltaTime,0));
             }
         }
         if((Input.GetKey("w") || Input.GetKey("up") || Input.GetKey("space")) && puedeSaltar)
         {
             puedeSaltar = false;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,fuerzaSalto));
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rb.AddForce(new Vector2(0,fuerzaSalto));
+        }
+        if(agarrado > 0)
+        {
+            if((Input.GetKey("a") || Input.GetKey("left")) && agarrado == 1)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb.AddForce(new Vector2(-(fuerzaMovimiento / 2) * Time.deltaTime,0));
+                agarrado = 0;
+            }
+            if((Input.GetKey("d") || Input.GetKey("right") && agarrado == 2))
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb.AddForce(new Vector2((fuerzaMovimiento / 2) * Time.deltaTime,0));
+                agarrado = 0;      
+            }
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.tag == "suelo" || collision.transform.tag == "plataforma")
+        if((collision.transform.tag == "suelo"))
         {
             puedeSaltar = true;
         }
-        if(collision.transform.tag == "agarradera" && !puedeSaltar)
+        else if(collision.transform.tag == "agarradera")
         {
-           
+            if(collision.transform.position.y + rb.transform.localScale.y > rb.transform.position.y)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                if(collision.transform.position.x < rb.transform.position.y)
+                {
+                    agarrado = 1;
+                }
+                else
+                {
+                    agarrado = 2;
+                }
+            }
+        }
+        else if(collision.transform.tag == "plataforma" && collision.transform.position.y + rb.transform.localScale.y < rb.transform.position.y)
+        {
+            puedeSaltar = true;
         }
     }
+
+    //private void agarrandose(Collision2D collision)
+    //{
+    //    bool ledgeOnLeft = gameObject.GetComponent<Rigidbody2D>().transform.position.x > collision.transform.position.x;
+    //    bool ledgeOnRight = !ledgeOnLeft;
+
+    //    if((Input.GetKey("down")|| Input.GetKey("s"))
+    //        || (mInputs[(int)KeyInput.GoLeft] && ledgeOnRight)
+    //        || (mInputs[(int)KeyInput.GoRight] && ledgeOnLeft))
+    //    {
+    //        if(ledgeOnLeft)
+    //            mCannotGoLeftFrames = 3;
+    //        else
+    //            mCannotGoRightFrames = 3;
+
+    //        mCurrentState = CharacterState.Jump;
+    //    }
+    //    else if(mInputs[(int)KeyInput.Jump])
+    //    {
+    //        mSpeed.y = mJumpSpeed;
+    //        mCurrentState = CharacterState.Jump;
+    //    }
+    //}
 }
