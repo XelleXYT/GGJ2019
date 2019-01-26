713309c9c0bf000b43b4c3200f4fbb3709180193
XelleX <33550473+XelleXYT@users.noreply.github.com>
2019-01-26 16:21:11 +0100

    Creada carpeta animations
    Mejorado el agarre de objetos.

--- Assets/Scripts/ObjectTransport.cs
+++ Assets/Scripts/ObjectTransport.cs
@@ -12,9 +12,22 @@ public class ObjectTransport : MonoBehaviour
     {
         if (enganchado)
         {
-            float posX = player.transform.position.x + player.transform.localScale.x / 2 + gameObject.transform.localScale.x / 2;
-            float posY = player.transform.position.y - player.transform.localScale.y / 2 + gameObject.transform.localScale.y / 2;
+            float posX;
+            if (player.transform.localPosition.x > gameObject.transform.localPosition.x)
+            {   
+                posX = player.transform.position.x - player.transform.localScale.x / 2 - gameObject.transform.localScale.x / 2;
+            }
+            else
+            {
+                posX = player.transform.position.x + player.transform.localScale.x / 2 + gameObject.transform.localScale.x / 2;
+            }
+            float posY = gameObject.transform.position.y;
             gameObject.transform.position = new Vector3(posX, posY, 0);
+
+            if ((Input.GetKey("s") || Input.GetKey("down") || Input.GetKey("w") || Input.GetKey("up") || Input.GetKey("space")))
+            {
+                enganchado = false;
+            }
         }
     }
 
