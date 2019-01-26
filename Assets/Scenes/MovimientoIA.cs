ed612d834b0e7aff21186dc17bac8fab85c3b6f4
alejandroLago <33934156+alejandroLago@users.noreply.github.com>
2019-01-26 19:30:42 +0100

    intentoDeIA

---
+++ Assets/Scenes/MovimientoIA.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoIA : MonoBehaviour
+{
+    float direccion;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "limitador")
+        {
+            direccion *= -1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector3(0, 0, 0));
+    }
+}
